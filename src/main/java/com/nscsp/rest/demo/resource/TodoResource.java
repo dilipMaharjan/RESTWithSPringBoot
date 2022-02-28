578abc88094c9d6ac3768a1a0356525ffc1bdad9
dilipMaharjan <dilipmaharjann@gmail.com>
2022-02-28 14:37:56 -0600

    open api for documentation setup

--- src/main/java/com/nscsp/rest/demo/resource/TodoResource.java
+++ src/main/java/com/nscsp/rest/demo/resource/TodoResource.java
@@ -2,6 +2,10 @@ package com.nscsp.rest.demo.resource;
 
 import com.nscsp.rest.demo.domain.Todo;
 import com.nscsp.rest.demo.service.TodoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +20,22 @@ import java.util.List;
 public class TodoResource {
     private TodoService todoService;
 
+    @Operation(
+            description = "Fetches all todos"
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Successfully fetched all data",
+                            content = @Content(mediaType = "application/json")
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Resource not found",
+                            content = @Content
+                    )}
+    )
     @GetMapping("/todos")
     public List<Todo> todos() {
         return todoService.todos();
